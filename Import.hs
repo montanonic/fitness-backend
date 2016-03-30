7876ac6f82b67495eada49ceb19e80b64f2594c1
Nicholas Montaño <montanonic@gmail.com>
2016-03-29 21:58:47 -0700

    Added some much needed utilities for the Maybe datatype.

--- Import.hs
+++ Import.hs
@@ -22,7 +22,43 @@ type PC val = (PersistEntity val, PersistEntityBackend val ~ SqlBackend)
 asSingleLine :: Text -> Text
 asSingleLine = mconcat . ((++ " ") <$>) . lines
 
--- | A version of maybe that takes the Maybe value first. I find my code would
--- often feel cleaner if I could use that value first.
-maybe' :: Maybe a -> b -> (a -> b) -> b
-maybe' m b f = maybe b f m
+--
+-- * Monads and Maybe utilities, courtesy of
+-- http://haddock.stackage.org/lts-5.9/Agda-2.4.2.5/src/Agda-Utils-Maybe.html#fromMaybeM
+--
+
+-- | Monadic version of 'maybe'.
+
+maybeM :: Monad m => m b -> (a -> m b) -> m (Maybe a) -> m b
+maybeM n j mm = maybe n j =<< mm
+
+-- | Monadic version of 'fromMaybe'.
+
+fromMaybeM :: Monad m => m a -> m (Maybe a) -> m a
+fromMaybeM m mm = maybeM m return mm
+
+-- | Monadic version of 'caseMaybe'.
+--   That is, 'maybeM' with a different argument ordering.
+caseMaybeM :: Monad m => m (Maybe a) -> m b -> (a -> m b) -> m b
+caseMaybeM mm d f = maybeM d f mm
+
+-- | 'caseMaybeM' with flipped branches.
+ifJustM :: Monad m => m (Maybe a) -> (a -> m b) -> m b -> m b
+ifJustM mm = flip (caseMaybeM mm)
+
+-- | A more telling name for 'Traversable.forM_' for the 'Maybe' collection type.
+--   Or: 'caseMaybe' without the 'Nothing' case.
+whenJust :: Monad m => Maybe a -> (a -> m ()) -> m ()
+whenJust m k = caseMaybe m (return ()) k
+
+-- | 'caseMaybe' without the 'Just' case.
+whenNothing :: Monad m => Maybe a -> m () -> m ()
+whenNothing m d = caseMaybe m d (\_ -> return ())
+
+-- | 'caseMaybeM' without the 'Nothing' case.
+whenJustM :: Monad m => m (Maybe a) -> (a -> m ()) -> m ()
+whenJustM c m = c >>= (`whenJust` m)
+
+-- | 'caseMaybeM' without the 'Just' case.
+whenNothingM :: Monad m => m (Maybe a) -> m () -> m ()
+whenNothingM mm d = mm >>= (`whenNothing` d)
